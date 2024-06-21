41e829c3d4e32bd181760dafecae0fe359acec59
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-06-21 18:01:27 -0400

    chore: Update global and page styles
    This commit updates the global CSS file to include a universal box-sizing rule and sets the margin and padding of all elements to 0. It also updates the page module CSS file to add styles for a square component, a grid layout, and a main container. Additionally, the page component is modified to use the updated styles and include a grid of square components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { Square } from "./square";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <div>
         <h1>Hello, world.</h1>
         <p>
           Alice is officially learning Next.js. <i>Sigh.</i>
         </p>
         <ReusableComponent />
+        <div className={styles.grid}>
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
       </div>
     </main>
   );
 }
 
 function ReusableComponent() {
-  return (
-    <p>This is a reusable component.</p>
-  )
+  return <p>This is a reusable component.</p>;
 }
