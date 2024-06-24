533f1f694cf32573027ee4728bbf1d80a0b3bd5f
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-06-24 08:48:44 -0400

    Shit's working.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,17 +1,17 @@
 // storage-adapter-import-placeholder
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import { fileURLToPath } from "url";
+import sharp from "sharp";
 
-import { Fuckers } from './collections/Fuckers'
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import { Fuckers } from "./collections/Fuckers";
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
@@ -19,15 +19,15 @@ export default buildConfig({
   },
   collections: [Fuckers, Users, Media],
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
+  secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
+    outputFile: path.resolve(dirname, "payload-types.ts"),
   },
   db: mongooseAdapter({
-    url: process.env.DATABASE_URI || '',
+    url: process.env.DATABASE_URI || "",
   }),
   sharp,
   plugins: [
     // storage-adapter-placeholder
   ],
-})
+});
