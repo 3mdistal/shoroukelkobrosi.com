64b3a0b293ef3bbc3bc88bf6119d02300dbdffc2
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-07-11 05:26:35 -0400

    All set. Time to bring in the design system.

--- apps/nextjs-playground/src/app/(app)/page.tsx
+++ apps/nextjs-playground/src/app/(app)/page.tsx
@@ -1,23 +1,7 @@
 export default function Home(): React.ReactElement {
   return (
     <div>
-      <p className="sm:font-display font-display text-8xl md:font-sans">
-        Display.
-      </p>
-      <p className="font-display text-7xl">Display.</p>
-      <p className="font-display text-6xl">Display.</p>
-      <p className="font-display-sm">Display.</p>
-      <p className="font-display-xs">Display.</p>
-      <p className="font-title-xl">Title.</p>
-      <p className="font-title-lg">Title.</p>
-      <p className="font-title-md">Title.</p>
-      <p className="font-title-sm">Title.</p>
-      <p className="font-title-xs">Title.</p>
-      <p className="font-body-xl">Body.</p>
-      <p className="font-body-lg">Body.</p>
-      <p className="font-body-md">Body.</p>
-      <p className="font-body-sm">Body.</p>
-      <p className="font-sans">Body.</p>
+      <p className="font-serif">Hello, world.</p>
     </div>
   );
 }
