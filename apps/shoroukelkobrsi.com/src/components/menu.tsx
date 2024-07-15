311b79716457157048b1e8c69d060df2d9fdc636
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-07-15 03:54:43 -0400

    Site styling is working.

--- apps/shoroukelkobrsi.com/src/components/menu.tsx
+++ apps/shoroukelkobrsi.com/src/components/menu.tsx
@@ -1,5 +1,13 @@
 import styles from "./menu.module.css";
 
 export default function Menu(): React.ReactElement {
-  return <div className={styles.testing} />;
+  return (
+    <div className={styles.menu}>
+      <div className={styles["menu-children"]}>Logo</div>
+      <div className={`${styles["menu-children"]} ${styles["menu-title"]}`}>
+        The cinema of Shorouk Elkorsi.
+      </div>
+      <div className={styles["menu-children"]}>Menu</div>
+    </div>
+  );
 }
