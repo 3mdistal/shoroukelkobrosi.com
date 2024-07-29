350e99eaa4671b1ecad152fcc1df978c9fec0aeb
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-07-29 16:22:09 -0400

    Film page fetching working.

--- apps/shoroukelkobrsi.com/src/app/(app)/films/[slug]/page.tsx
+++ apps/shoroukelkobrsi.com/src/app/(app)/films/[slug]/page.tsx
@@ -1,3 +1,39 @@
-export default function FilmPage(): React.ReactElement {
-  return <p>Film Page</p>;
+import { getPayloadHMR } from "@payloadcms/next/utilities";
+import type { Payload } from "payload";
+import { unstable_cache as cache } from "next/cache";
+import configPromise from "@payload-config";
+import type { Film } from "@/payload-types";
+
+const getCachedFilm = cache(
+  async (slug: string): Promise<Film> => {
+    const payload: Payload = await getPayloadHMR({
+      config: configPromise,
+    });
+
+    const response = await payload.find({
+      collection: "films",
+      where: {
+        slug: {
+          equals: slug,
+        },
+      },
+    });
+
+    return response.docs[0];
+  },
+  ["film-cache"],
+  {
+    tags: [`film`],
+  },
+);
+
+export default async function FilmPage({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<React.ReactElement> {
+  const filmSlug = params.slug;
+  const film = await getCachedFilm(filmSlug);
+
+  return <h1>Film Page: {film.title}</h1>;
 }
