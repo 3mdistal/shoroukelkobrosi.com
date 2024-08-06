987bf9f6f4bd13474ab6a35d286518d590d1ab09
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-06 17:02:56 -0400

    Menu fixed.

--- apps/shoroukelkobrsi.com/src/components/menu.tsx
+++ apps/shoroukelkobrsi.com/src/components/menu.tsx
@@ -4,9 +4,10 @@ import styles from "./menu.module.css";
 export default function Menu(): React.ReactElement {
   return (
     <header className={styles.menu}>
-      <div className={styles.menuLogo}>Logo</div>
+      <Link href="/" className={styles.menuLogo}>
+        <div>Logo</div>
+      </Link>
       <nav className={styles.menuNav}>
-        <Link href="/">Home</Link>
         <Link href="/stills">Stills</Link>
         <button type="button" className={styles.menuButton}>
           Menu
