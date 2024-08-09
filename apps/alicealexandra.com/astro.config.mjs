26527b1481e25241f9a73bec95e48d637b42eb88
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-09 08:29:55 -0400

    Added Svelte.

--- apps/alicealexandra.com/astro.config.mjs
+++ apps/alicealexandra.com/astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
-
 import sitemap from "@astrojs/sitemap";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alicealexandra.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte()],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: {
