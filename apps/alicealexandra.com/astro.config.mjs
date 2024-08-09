c99dacf38bae9e106da9bc92afc2e2d8e45141e4
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-09 08:16:35 -0400

    Sitemap addded.

--- apps/alicealexandra.com/astro.config.mjs
+++ apps/alicealexandra.com/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: "https://alicealexandra.com",
+  integrations: [mdx(), sitemap()],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: {
