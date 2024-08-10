0064bb9ebf1bea17bd056368af9e1035db39f8aa
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-10 12:36:17 -0400

    Recipe data working well.

--- apps/mini/meal-planner/src/content/config.ts
+++ apps/mini/meal-planner/src/content/config.ts
@@ -2,12 +2,15 @@ import { defineCollection, z } from "astro:content";
 
 const recipeCollection = defineCollection({
   type: "data",
-  schema: z.array(
-    z.object({
-      name: z.string(),
-      type: z.enum(["breakfast", "dinner", "snack", "dessert", "fruit"]),
-    }),
-  ),
+  schema: z.object({
+    $schema: z.string().optional(),
+    data: z.array(
+      z.object({
+        name: z.string(),
+        type: z.enum(["breakfast", "dinner", "snack", "dessert", "fruit"]),
+      }),
+    ),
+  }),
 });
 
 export const collections = {
