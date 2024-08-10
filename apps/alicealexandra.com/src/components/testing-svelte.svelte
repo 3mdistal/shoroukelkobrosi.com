bd7b501756e9a9d2047a721ddc4a20c8f3436508
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-10 09:23:03 -0400

    Studio card.

--- apps/alicealexandra.com/src/components/testing-svelte.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  const name = "Alice";
-  let count = $state(0);
-</script>
-
-<p>Hello {name}, this is a Svelte component.</p>
-<button onclick={() => count++}>{count}</button>
