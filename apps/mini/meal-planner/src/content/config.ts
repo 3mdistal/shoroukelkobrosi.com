fb3d94d6cdd10cdde9eb816ccac08f65f9b14ef0
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-11 14:32:42 -0400

    Added more recipes.

--- apps/mini/meal-planner/src/content/config.ts
+++ apps/mini/meal-planner/src/content/config.ts
@@ -1,22 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
+const schemaPath =
+  "/Users/monkey/Downloads/Coding Projects/teenylilapps/apps/mini/meal-planner/.astro/collections/";
+
 const recipeCollection = defineCollection({
   type: "data",
   schema: z.object({
-    $schema: z.string().optional(),
-    data: z.array(
-      z.object({
-        name: z.string(),
-        type: z.enum([
-          "breakfast",
-          "dinner",
-          "side",
-          "snack",
-          "dessert",
-          "fruit",
-        ]),
-      }),
-    ),
+    $schema: z.literal(`${schemaPath}recipes.schema.json`),
+    name: z.string(),
+    type: z.enum(["breakfast", "dinner", "side", "snack", "dessert", "fruit"]),
   }),
 });
 
