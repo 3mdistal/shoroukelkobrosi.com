f77b2dea92755f746f80b83c5f3fb95b17f809e8
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-14 04:11:26 -0400

    Prepping for changes.

--- apps/shoroukelkobrsi.com/src/components/about/about-professional-photo.tsx
+++ apps/shoroukelkobrsi.com/src/components/about/about-professional-photo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import getAboutPage from "@/components/about/get-about-page";
 import { getImageDimensions, createImageUrl } from "@/utilities/media";
 
-export default async function AboutPersonalPhoto(): Promise<React.ReactElement> {
+export default async function AboutProfessionalPhoto(): Promise<React.ReactElement> {
   const aboutPage = await getAboutPage();
   const image = aboutPage["professional-photo"];
   const { width, height } = getImageDimensions(image);
