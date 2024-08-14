68baa7c63b756ed78d9b7135fb096a05186777ea
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-14 03:55:25 -0400

    Photos added.

--- apps/shoroukelkobrsi.com/src/app/(app)/about/page.tsx
+++ apps/shoroukelkobrsi.com/src/app/(app)/about/page.tsx
@@ -1,6 +1,8 @@
 import AboutPersonalIntro from "@/components/about/about-personal-intro";
 import AboutProfessionalIntro from "@/components/about/about-professional-intro";
 import AboutProfessionalLogos from "@/components/about/about-professional-logos";
+import AboutPersonalPhoto from "@/components/about/about-personal-photo";
+import AboutProfessionalPhoto from "@/components/about/about-professional-photo";
 
 export default function AboutPage(): React.ReactElement {
   return (
@@ -8,6 +10,8 @@ export default function AboutPage(): React.ReactElement {
       <AboutPersonalIntro />
       <AboutProfessionalIntro />
       <AboutProfessionalLogos />
+      <AboutPersonalPhoto />
+      <AboutProfessionalPhoto />
     </>
   );
 }
