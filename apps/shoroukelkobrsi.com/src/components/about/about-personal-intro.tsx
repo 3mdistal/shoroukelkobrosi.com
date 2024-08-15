ccc75bfde7e8dc92558faa39628e4e85cb9ca8f8
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-15 04:06:27 -0400

    Content toggle iterations.

--- apps/shoroukelkobrsi.com/src/components/about/about-personal-intro.tsx
+++ apps/shoroukelkobrsi.com/src/components/about/about-personal-intro.tsx
@@ -1,8 +1,14 @@
 import getAboutPage from "./get-about-page";
+import styles from "./about-intro.module.css";
 
 export default async function AboutPersonalIntro(): Promise<React.ReactElement> {
   const aboutPage = await getAboutPage();
   const introHtml = aboutPage["personal-intro_html"];
 
-  return <div dangerouslySetInnerHTML={{ __html: introHtml ?? "" }} />;
+  return (
+    <div
+      className={styles.container}
+      dangerouslySetInnerHTML={{ __html: introHtml ?? "" }}
+    />
+  );
 }
