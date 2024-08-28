53a3236a6f2677cc539c14e8e4d03461f9d044e4
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-28 04:11:34 -0400

    Video working, quick and dirty.

--- apps/shoroukelkobrsi.com/src/components/film/reel.tsx
+++ apps/shoroukelkobrsi.com/src/components/film/reel.tsx
@@ -1,9 +1,38 @@
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import { unstable_cache as cache } from 'next/cache'
+import configPromise from '@payload-config'
+import type { Homepage } from '@/payload-types'
+import type { Payload } from 'payload'
+import { createImageUrl } from '@/utilities/media'
 import styles from './reel.module.css'
 
-export default function Reel(): React.ReactElement {
+const getCachedHomepage = cache(
+  async (): Promise<Homepage> => {
+    const payload: Payload = await getPayloadHMR({
+      config: configPromise,
+    })
+
+    const homepage = await payload.findGlobal({
+      slug: 'homepage',
+    })
+
+    return homepage
+  },
+  ['homepage-cache'],
+  {
+    tags: ['homepage'],
+  },
+)
+
+export default async function Reel(): Promise<React.ReactElement> {
+  const homepage = await getCachedHomepage()
+  const reel = homepage.reel
+
   return (
     <div className={styles.reelContainer}>
-      <div className={styles.reel} />
+      <video className={styles.reel} autoPlay muted loop>
+        <source src={createImageUrl(reel)} type="video/mp4" />
+      </video>
     </div>
   )
 }
