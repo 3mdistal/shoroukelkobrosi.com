9bf86f32624ffc8269341a7a11bbf3e943c39946
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-10-22 03:25:29 -0400

    Footer layout fixed a bit.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,40 +7,42 @@ export default function Footer(): React.ReactElement {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <div className={styles.footerLogo}>
-          {/* Placeholder for logo */}
-          <span>LOGO</span>
+        <div className={styles.footerSubGrid}>
+          <nav className={styles.footerNav}>
+            <p style={{ fontWeight: 'bold' }}>Explore</p>
+            <Link href="/">Films</Link>
+            <Link href="/about">About</Link>
+            <Link href="/stills">Stills</Link>
+            <Link href="/blog">Blog</Link>
+          </nav>
+          <div className={styles.footerSocial}>
+            <p style={{ fontWeight: 'bold' }}>Get in touch</p>
+            <a
+              href="https://www.instagram.com/anthropotpourri"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+            <a href="https://vimeo.com/shoroukelkobrosi" target="_blank" rel="noopener noreferrer">
+              Vimeo
+            </a>
+            <a
+              href="https://www.imdb.com/name/nm15338675"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              IMDb
+            </a>
+          </div>
         </div>
         <div className={styles.footerCopyright}>
+          <p style={{ fontWeight: 'bold' }}>Legal</p>
           <Link href="/legal">
             &copy; {currentYear} Shorouk Elkobrosi.
             <br />
             All rights reserved.
           </Link>
-        </div>
-        <nav className={styles.footerNav}>
-          <Link href="/">Films</Link>
-          <Link href="/about">About</Link>
-          <Link href="/stills">Stills</Link>
-        </nav>
-        <div className={styles.footerSocial}>
-          <a
-            href="https://www.instagram.com/anthropotpourri"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Instagram
-          </a>
-          <a href="https://vimeo.com/shoroukelkobrosi" target="_blank" rel="noopener noreferrer">
-            Vimeo
-          </a>
-          <a href="https://www.imdb.com/name/nm15338675" target="_blank" rel="noopener noreferrer">
-            IMDb
-          </a>
-        </div>
-        <div className={styles.footerToggle}>
-          {/* Placeholder for light/dark toggle */}
-          <button>Toggle Theme</button>
         </div>
       </div>
     </footer>
